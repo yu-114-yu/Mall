2e957a17347167e88e3a98b1eb630a95629dae7c
Yu <15880377720@163.com>
2020-08-14 17:06:20 +0800

    正则完成

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,7 @@ module.exports = {
       selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item'],//指定不需要转换的类
       minPixelValue: 1,   //小于或等于“1px不转换为视窗单位，
       mediaQuery: false,   //允许在媒体查询中转换、px
-      exclude: [/TabBar/]  //必须是正则，匹配文件的0000
+      exclude: [/TabBar/]  //必须是正则，匹配文件的
     }
   }
 }
